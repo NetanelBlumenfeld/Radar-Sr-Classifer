1657f5df1f10084723e010683d89edd6230c403a
NetanelBlumenfeld <110423835+NetanelBlumenfeld@users.noreply.github.com>
2024-07-16 11:28:23 +0300

    4

--- python/gestures/main.py
+++ python/gestures/main.py
@@ -17,7 +17,7 @@ from gestures.utils_processing_data import (
 if __name__ == "__main__":
 
     pc, data_dir, output_dir, device = get_pc_cgf()
-    task = "classifier"  # task = ["sr", "classifier", "sr_classifier"]
+    task = "sr_classifier"  # task = ["sr", "classifier", "sr_classifier"]
     original_dims = True if task == "classifier" else False
     for x in [1]:
         for dim in [24, 36]:
